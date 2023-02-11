ec43b4dbb9e04f819eefddab592fbb50f00d3bca
alex <thegamerale97@gmail.com>
2023-02-11 10:37:51 +0100

    Aggiungere i file di progetto.

---
+++ DB/DBUpload.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace sql_interface_net_wpf.DB
+{
+    internal class DBUpload
+    {
+        private string conn_string;
+        public DBUpload()
+        {
+            conn_string = string.Empty;
+        }
+
+        public DBUpload(string conn_string)
+        {
+            this.conn_string = conn_string;
+        }
+
+        public void Query(string command)
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection();
+                MySqlCommand comm = new MySqlCommand();
+                conn.ConnectionString = conn_string;
+                conn.Open();
+                comm.CommandText = command;
+                comm.Connection = conn;
+                comm.ExecuteNonQuery();
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using sql_interface_net_wpf.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sql_interface_net_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connetionString;
+            connetionString = "Server=127.0.0.1,3306;Database=xela;User Id=xela;Password=xela;";
+
+            DBUpload up = new DBUpload(connetionString);
+            up.Query("CREATE TABLE IF NOT EXISTS Students (name TEXT(20), surname TEXT(20))");
+
+        }
+    }
+}
