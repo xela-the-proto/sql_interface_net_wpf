35f240a03b26a11913d985da8fa4d2ea2e3cbdf5
alex <thegamerale97@gmail.com>
2023-06-19 11:52:52 +0200

    Fixed a bug where the wizard menu would remain open

--- Windows/ConnectionWizard.xaml.cs
+++ Windows/ConnectionWizard.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
+using sql_interface_net_wpf;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using xelas_not_so_convenient_mysql_interface.JSONClasses;
 
@@ -17,15 +19,17 @@ namespace xelas_not_so_convenient_mysql_interface.Windows
 
         private void write_button_Click(object sender, RoutedEventArgs e)
         {
+            ConnectionWizard wizard = Application.Current.Windows.OfType<ConnectionWizard>().FirstOrDefault();
             Connection connection = new Connection(db_ip.Text, db_name.Text, username.Text, username_password.Password);
             using (StreamWriter file = File.CreateText(".\\Config\\connection.json"))
             {
                 JsonSerializer serializer = new JsonSerializer();
                 serializer.Serialize(file, connection);
-                MessageBox.Show("wrote to file");
             }
 
             MessageBox.Show("Wrote to file!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            
+            wizard.Close();
         }
     }
 }
